f75f6d9118617f29645b9f98997aa8075218d35d
FraAccaBi <francescahuda@gmail.com>
2022-03-29 16:35:03 +0200

    complete game section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,21 @@
 /* Mail
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-Consigli del giorno:
-scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-si ma noi cosa vogliamo fare?
-torniamo a scrivere in italiano
-proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-non bisogna ricordarsi a memoria come generare dei numeri random, c'é W3S per questo https://www.w3schools.com/js/js_random.asp
-BONUS:
-Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina. */
+ */
 
 
 //creo una whitelist per le mail accettate
 const whitelist = ["ciccio@gmail.com", "franco@gmail.com"]
 
-
-//creo una variabile associata la mail dell'user
-
+//creo una variabile con la mail dell'utente tramite input 
 const userMail = prompt('inserisci mail');
-const dim_whitelist = whitelist.length
-/* document.getElementById('user_mail') */
-//confronto la variabile ottenuta con la mia whitelist
 
+//calcolo la dimensione dell'array che mi servirà nella condizione del ciclo for
+const dim_whitelist = whitelist.length
+
+//creo una variabile presence di valore false che diventerà true  qualora la mail fosse presente nell'arrai per permettermi di uscire dal ciclo
 let presence = false;
 
+//confronto la variabile ottenuta con la mia whitelist
 for (let i = 0; i < dim_whitelist; i++) {
     if (userMail == whitelist[i]) {
         console.log('iscrizione avvenuta con successo');
@@ -34,6 +23,37 @@ for (let i = 0; i < dim_whitelist; i++) {
     } 
 }
 
+//in caso di assenza della mail nell'array restituisco un messaggio in console
 if(!presence){
     console.log('la tua mail non è abilitata');
 }
+
+
+
+
+
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+//genero un vaolre random per il giocatore
+const player_number = Math.ceil(Math.random()*6)
+console.log(player_number);
+
+//genero un valore random per il pc
+const pc_number = Math.ceil(Math.random()*6)
+console.log(pc_number);
+
+//confronto
+if (player_number > pc_number) {
+    console.log('hai vinto');
+} else if (player_number == pc_number){
+    console.log('pareggio');
+} else {
+    console.log('hai perso');
+}
+
+
+/* 
+BONUS:
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina. */
