8d54c79474a9128d5b29ea8f997d0fbdd23283db
FraAccaBi <francescahuda@gmail.com>
2022-03-29 16:56:51 +0200

    BOUNS add html to mail section

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,27 +6,39 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 //creo una whitelist per le mail accettate
 const whitelist = ["ciccio@gmail.com", "franco@gmail.com"]
 
-//creo una variabile con la mail dell'utente tramite input 
-const userMail = prompt('inserisci mail');
-
 //calcolo la dimensione dell'array che mi servirà nella condizione del ciclo for
 const dim_whitelist = whitelist.length
 
 //creo una variabile presence di valore false che diventerà true  qualora la mail fosse presente nell'arrai per permettermi di uscire dal ciclo
 let presence = false;
+ 
+//creo un bottone 
+const button_input = document.getElementById('inserisci')
 
-//confronto la variabile ottenuta con la mia whitelist
-for (let i = 0; i < dim_whitelist; i++) {
-    if (userMail == whitelist[i]) {
-        console.log('iscrizione avvenuta con successo');
-        presence =true;
-    } 
-}
+//creo un listener che reagisce al click sul bottone
+button_input.addEventListener('click', function(){
 
-//in caso di assenza della mail nell'array restituisco un messaggio in console
-if(!presence){
-    console.log('la tua mail non è abilitata');
-}
+    //creo una variabile con la mail dell'utente tramite input
+    const userMail = document.getElementById('user_mail').value 
+    //confronto la variabile ottenuta con la mia whitelist
+    for (let i = 0; i < dim_whitelist; i++) {
+        if (userMail == whitelist[i]) {
+            document.getElementById('resultMail').innerHTML ='iscrizione avvenuta con successo';
+            presence =true;
+        } 
+    }
+
+    //in caso di assenza della mail nell'array restituisco un messaggio in console
+    if(!presence){
+        document.getElementById('resultMail').innerHTML = 'la tua mail non è abilitata';
+    }
+})
+
+
+
+
+
+
 
 
 
